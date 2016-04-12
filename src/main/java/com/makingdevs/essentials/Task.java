3d1fa122252c414881a26139078c9065fd34f67e
josdem <joseluis.delacruz@gmail.com>
2016-04-12 18:19:31 -0500

    Setup inicial

---
+++ src/main/java/com/makingdevs/essentials/Task.java
@@ -0,0 +1,5 @@
+package com.makingdevs.essentials;
+
+public class Task {
+
+}
---
+++ src/main/java/com/makingdevs/essentials/TaskManager.java
@@ -0,0 +1,32 @@
+package com.makingdevs.essentials;
+
+import java.util.List;
+
+public class TaskManager {
+
+  public int howManyTasks() {
+    return 0;
+  }
+
+  public void addTask(Task task) {
+  }
+
+  public void addTask(String s) {
+  }
+
+  public void addTask(List<Task> tasksToAdd) {
+
+  }
+
+  public Task getTaskAt(int i) {
+    return null;
+  }
+
+  public Task findTask(String s) {
+    return null;
+  }
+
+  public List<Task> findTasks(String s) {
+    return null;
+  }
+}
